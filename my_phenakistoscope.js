87987916d056e3e057fbb18c95946403b787c8d7
huihui-543 <151787971+huihui-543@users.noreply.github.com>
2024-01-10 18:04:32 +1300

    Update my_phenakistoscope.js

--- my_phenakistoscope.js
+++ my_phenakistoscope.js
@@ -25,26 +25,11 @@ function setup_layers(pScope){
 function faces(x, y, animation, pScope){
   pScope.draw_image("ghost",x,y);
   scale(animation.frame*2);
-
+  
   ellipse(0,0,50,50); // draw head
   fill(30);
   ellipse(-10,-10,10,10); //draw eye
   ellipse(10,-10,10,10); // draw eye
   arc(0,10,20,10,0,180); // draw mouth
-
-}
-
-function squares(x, y, animation, pScope){
-
-  // this is how you set up a background for a specific layer
-  let angleOffset = (360 / SLICE_COUNT) / 2
-  let backgroundArcStart = 270 - angleOffset;
-  let backgroundArcEnd = 270 + angleOffset;
-
-  fill(66, 135, 245)
-  arc(x,y,800,800,backgroundArcStart,backgroundArcEnd); // draws "pizza slice" in the background
-
-  fill(255)
-  rect(-10,-300-animation.wave()*50,20,20) // .wave is a cosine wave btw
 
 }
