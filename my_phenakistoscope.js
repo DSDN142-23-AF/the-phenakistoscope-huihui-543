3da751e7ea0ad22497a2abf7adb8e2755ebbb383
huihui-543 <151787971+huihui-543@users.noreply.github.com>
2024-01-12 10:26:38 +1300

    Update my_phenakistoscope.js

--- my_phenakistoscope.js
+++ my_phenakistoscope.js
@@ -13,17 +13,23 @@ function setup_layers(pScope){
 
   new PLayer(null, 220);  //lets us draw the whole circle background, ignoring the boundaries
 
-  var layer1 = new PLayer(heart_circle);
+  var layer1 = new PLayer(faces);
   layer1.mode( SWIRL(5) );
   layer1.set_boundary( 200, 1000 );
 
-  var layer2 = new PLayer(heart_circle);
-  layer2.mode( RING );
-  layer2.set_boundary( 0, 400 );
+  // var layer2 = new PLayer(squares);
+  // layer2.mode( RING );
+  // layer2.set_boundary( 0, 400 );
 }
 
 function faces(x, y, animation, pScope){
   pScope.draw_image("heart_circle",x,y);
   scale(animation.frame*2);
+  
+  ellipse(0,0,50,50); // draw head
+  fill(30);
+  ellipse(-10,-10,10,10); //draw eye
+  ellipse(10,-10,10,10); // draw eye
+  arc(0,10,20,10,0,180); // draw mouth
 
 }
