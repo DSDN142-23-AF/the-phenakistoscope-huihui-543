c78208aa95dd2362677fffab0d71d733479a95cc
huihui-543 <151787971+huihui-543@users.noreply.github.com>
2024-01-12 18:01:28 +1300

    Update my_phenakistoscope.js

--- my_phenakistoscope.js
+++ my_phenakistoscope.js
@@ -1,7 +1,7 @@
 const SLICE_COUNT = 10;
 
 function setup_pScope(pScope){
-  pScope.output_mode(ANIMATED_DISK);
+  pScope.output_mode(OUTPUT_GIF(1000));
   pScope.scale_for_screen(true);
   pScope.draw_layer_boundaries(false);
   pScope.set_direction(CCW);
@@ -16,27 +16,26 @@ function setup_layers(pScope){
 
   var cupidSequence = new PLayer(cupidLayer);
   cupidSequence.mode( SWIRL(2) );
-  cupidSequence.set_boundary(200, 1000 );
+  cupidSequence.set_boundary(200, 950);
 
   var layer2 = new PLayer(heartLayer);
-   layer2.mode( RING );
-   layer2.set_boundary( 0, 400 );
-
-function flowerLayer(x, y, pScope) {
-  drawflower(x, y, 100);
-
-function heartLayer(x, y, animation, pScope)
-pScope.draw_image("heart",x,y);
-}
+   layer2.mode( SWIRL (1) );
+   layer2.set_boundary( 0, 1100 );
 
 var flower = new PLayer(flowerLayer);
 flower.set_boundary(0, 200);
 }
 
+function flowerLayer(x, y, pScope) {
+  drawflower(x, y, 100);
+}
 function cupidLayer(x, y, animation, pScope){
   pScope.draw_image_from_sequence("cupid", 0, -50, animation.frame);
 }
-
+function heartLayer(x, y, animation, pScope){
+  pScope.draw_image("heart",300,1000);
+  rotate(360*animation.frame);
+}
 function drawflower(x, y, size) {
   fill(255, 0, 0); // red
   beginShape();
@@ -45,6 +44,7 @@ function drawflower(x, y, size) {
   bezierVertex(x + size * 2, y - size, x + size * 1.5, y - size / 2, x + size / 2, y + size / 4); // 
   endShape(CLOSE);
 }
+
 
 
 
