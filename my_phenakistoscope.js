d477205ffc3228a8a5b7eb7ee46612153fce0e60
huihui-543 <151787971+huihui-543@users.noreply.github.com>
2024-01-12 13:33:38 +1300

    change

--- my_phenakistoscope.js
+++ my_phenakistoscope.js
@@ -8,7 +8,9 @@ function setup_pScope(pScope){
   pScope.set_slice_count(10);
   pScope.load_image("heart_circle" , "png");
   pScope.load_image_sequence("cupid" , "png", 10);
+  
 }
+
 
 
 function setup_layers(pScope){
@@ -16,17 +18,34 @@ function setup_layers(pScope){
   new PLayer(null, 220);  //lets us draw the whole circle background, ignoring the boundaries
 
   var cupidSequence = new PLayer(cupidLayer);
-  cupidSequence.mode( SWIRL(3) );
-  cupidSequence.set_boundary( 200, 1000 );
+  cupidSequence.mode( SWIRL(2) );
+  cupidSequence.set_boundary(200, 1000 );
 
   // var layer2 = new PLayer(squares);
   // layer2.mode( RING );
   // layer2.set_boundary( 0, 400 );
+
+function heartLayer(x, y, pScope) {
+  drawHeart(x, y, 100);
+}
+
+var heart = new PLayer(heartLayer);
+heart.set_boundary(0, 200);
 }
 
 
 function cupidLayer(x, y, animation, pScope){
-  pScope.draw_image_from_sequence("cupid", 0, -100, animation.frame);
+  pScope.draw_image_from_sequence("cupid", 0, -50, animation.frame);
 }
+
+function drawflower(x, y, size) {
+  fill(255, 0, 0); // Set the fill color to red
+  beginShape();
+  vertex(x + size / 5, y + size / 100); // 
+  bezierVertex(x - size / 10, y - size / 2, x - size / 4, y - size, x + size / 2, y - size);
+  bezierVertex(x + size * 2, y - size, x + size * 1.5, y - size / 2, x + size / 2, y + size / 4); // 
+  endShape(CLOSE);
+}
+
 
 
